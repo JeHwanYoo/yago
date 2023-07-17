8dca6e3099574f912ce0d1f976c5c28c50db3c18
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-07-17 14:22:53 +0900

    Add: injectable.go
    Define Parser, Generator, and Writer interfaces for dependency injection.

---
+++ src/yago/injectable/injectable.go
@@ -0,0 +1,15 @@
+package injectable
+
+import "os"
+
+type Parser interface {
+	Parse([]byte) (interface{}, error)
+}
+
+type Generator interface {
+	Generate(interface{}) (string, error)
+}
+
+type Writer interface {
+	WriteFile(name string, data []byte, perm os.FileMode) error
+}
